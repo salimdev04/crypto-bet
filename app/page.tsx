cd2c7d53c10fe762e38f6fa94cc87a1be6915ed9
Salimdev <Salimdev04@gmail.com>
2023-09-06 14:43:53 +0100

    First commit...

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <header className="flex justify-between p-8 border">
+        <h1 className=" font-bold text-lg">Crypto Bet</h1>
+        <button>Get Started</button>
+      </header>
+    </main>
+  );
+}
