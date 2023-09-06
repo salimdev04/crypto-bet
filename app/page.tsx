c132ce9ef19fd48d18f55729a1d68645fef5976c
Salimdev <Salimdev04@gmail.com>
2023-09-06 15:04:50 +0100

    First commit...

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
+"use client";
+
+import { ethers } from "ethers";
 import Image from "next/image";
 
 export default function Home() {
+  const connexion = () => {
+    console.log("connect metamask");
+  };
+
   return (
     <main className="">
       <header className="flex justify-between p-8 border">
         <h1 className=" font-bold text-lg">Crypto Bet</h1>
-        <button>Get Started</button>
+        <h1 className=" font-bold text-lg">Crypto Fantasy</h1>
+        <button onClick={() => connexion()} className="p-2 border ">
+          Get Started
+        </button>
       </header>
     </main>
   );
