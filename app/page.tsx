e8aca1aba0f8ae4b1f46ff040eaaa0ee53b7c81e
Salimdev <Salimdev04@gmail.com>
2023-09-06 15:16:47 +0100

    get user address

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,16 @@ import { ethers } from "ethers";
 import Image from "next/image";
 
 export default function Home() {
-  const connexion = () => {
+  const connexion = async () => {
     console.log("connect metamask");
-  };
+    //@ts-ignore
+    const provider = new ethers.BrowserProvider(window.ethereum);
 
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const myAddress = (await signer).getAddress();
+    console.log(myAddress);
+  };
   return (
     <main className="">
       <header className="flex justify-between p-8 border">
